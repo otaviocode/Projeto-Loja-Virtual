771cb05ac0828bed7ea30d5e9e6021824f7214c8
otaviocode <luizgomesdossantos1970@gmail.com>
2025-03-14 22:15:07 -0300

    Commit atual 14-03

--- src/main/java/jdev/mentoria/lojavirtual/security/WebConfigSecurity.java
+++ src/main/java/jdev/mentoria/lojavirtual/security/WebConfigSecurity.java
@@ -2,22 +2,39 @@ package jdev.mentoria.lojavirtual.security;
 
 import javax.servlet.http.HttpSessionListener;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import jdev.mentoria.lojavirtual.service.ImplementacaoUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-/* Observation: I'm not sure if the implement is from Javax, if be wrong change this. */
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements HttpSessionListener {
-	
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso", "/deleteAcesso").antMatchers(HttpMethod.POST, "/salvarAcesso", "/deleteAcesso");
-	} 
 
+	@Autowired
+	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
+
+	/* Irá consultar o user no banco com Spring Security */
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(implementacaoUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+
+	}
+
+	/* Ignora alguas URL livre de autenticação */
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso", "/deleteAcesso").antMatchers(HttpMethod.POST,
+				"/salvarAcesso", "/deleteAcesso");
+		/* Ingnorando URL no momento para nao autenticar */
+	}
 
 }
